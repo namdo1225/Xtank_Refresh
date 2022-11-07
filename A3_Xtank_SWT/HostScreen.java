dbfd4e965f7b42bd2643b5c0a91cd7458a8467f2
Nam <namdo1204@gmail.com>
2022-11-06 22:09:34 -0700

    20221106: Map selection added to HostScreen.

--- A3_Xtank_SWT/HostScreen.java
+++ A3_Xtank_SWT/HostScreen.java
@@ -8,6 +8,7 @@ import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
@@ -20,29 +21,39 @@ public class HostScreen extends Screen {
 	
 	private Button		update;
 	private Button		next;
-	private Button		back;
+	private Button		backServer;
 	private Button		updatePlayer;
 	
 	private Composite	compositeNetwork;
 	
 	
 	private Composite	compositeMap;
+	private Group		mapGroup;
 	
+	private Button		map1;
+	private Button		map2;
+	
+	private Button		selectMap;
+	private Button		backMap;
+	
+	private int			mapID;
 	
 	public HostScreen(Shell shell, Display display, ClientController cCon, HostController hCon,
 			ClientModel cMod, HostModel hMod) {
 		super(shell, display, cCon, hCon, cMod, hMod);
+		
+		mapID = 1;
 	}
 	
 	@Override
 	protected Composite makeComposite(Shell shell, Display display) {
 		composite = new Composite(shell, SWT.COLOR_WHITE);
 		
-		compositeNetwork = new Composite(composite, SWT.TRANSPARENT);
 		compositeMap = new Composite(composite, SWT.TRANSPARENT);
+		compositeNetwork = new Composite(composite, SWT.TRANSPARENT);
 		
-		compositeNetwork.setLayout(new FillLayout(SWT.VERTICAL));
 		compositeMap.setLayout(new FillLayout(SWT.VERTICAL));
+		compositeNetwork.setLayout(new FillLayout(SWT.VERTICAL));
 		
 		String address = "";
 		try {
@@ -54,13 +65,37 @@ public class HostScreen extends Screen {
 				+ "\nYou cannot click 'Next' to start the game "
 				+ "unless 2 or more players are in.\nYour input is validated. "
 				+ "Program will crash if your number is out of int's bounds.\nYou cannot host AND play the game."
-				+ "\nWell-known port range: 1024-65,535\tLocalhost: 127.0.0.1\nYour (probably wrong) IP: "
+				+ "\n\nWell-known port range: 1024-65,535\tLocalhost: 127.0.0.1\nYour (probably wrong) IP: "
 				+ address
-				+ "\nPick a map:");
+				+ "\n\nPick a map FIRST! You cannot change your selection once it is chosen:");
 		guide.setFont(new Font(display,"Times New Roman", 12, SWT.BOLD ));
 		guide.setAlignment(SWT.CENTER);
 		
+		mapGroup = new Group(compositeMap, SWT.NONE);
+		mapGroup.setLayout(new FillLayout(SWT.HORIZONTAL));
 		
+		map1 = new Button(mapGroup, SWT.RADIO);
+		map1.setText("Map 1");
+		map1.setSelection(true);
+		map1.addSelectionListener(
+				SelectionListener.widgetSelectedAdapter(e-> mapID = 1));
+		
+		map2 = new Button(mapGroup, SWT.RADIO);
+		map2.setText("Map 2");
+		map1.addSelectionListener(
+				SelectionListener.widgetSelectedAdapter(e-> mapID = 2));
+		
+		selectMap = new Button(compositeMap, SWT.PUSH);
+		selectMap.setText("Select Map");
+		selectMap.addSelectionListener(
+				SelectionListener.widgetSelectedAdapter(e-> mapSelected()));
+		
+		backMap = new Button(compositeMap, SWT.PUSH);
+		backMap.setText("Back");
+		backMap.addSelectionListener(
+				SelectionListener.widgetSelectedAdapter(e-> closeServer()));
+		
+		compositeNetwork.setEnabled(false);
 		
 		numPlayers = new Label(compositeNetwork, SWT.BALLOON);
 		numPlayers.setFont(new Font(display,"Times New Roman", 14, SWT.BOLD ));
@@ -84,9 +119,9 @@ public class HostScreen extends Screen {
 		next.setText("Next");
 		next.addSelectionListener(SelectionListener.widgetSelectedAdapter(e-> validateInput()));
 		
-		back = new Button(compositeNetwork, SWT.PUSH);
-		back.setText("Back");
-		back.addSelectionListener(
+		backServer = new Button(compositeNetwork, SWT.PUSH);
+		backServer.setText("Back");
+		backServer.addSelectionListener(
 				SelectionListener.widgetSelectedAdapter(e-> closeServer()));
 		
 		composite.setLayout(new FillLayout(SWT.HORIZONTAL));
@@ -94,17 +129,23 @@ public class HostScreen extends Screen {
 		return composite;
 	}
 
+	private void mapSelected() {
+		compositeNetwork.setEnabled(true);
+		compositeMap.setEnabled(false);
+		selectMap.setText("YOU CAN NOW CLICK ON THE SCREEN TO THE RIGHT.");
+	}
+	
 	private void hostServer() {
 		if (validateInput())
 			try {
-				hControl.createServer(Integer.parseInt(port.getText()), 2);
+				hControl.createServer(Integer.parseInt(port.getText()), mapID);
 			} catch (Exception e) {}
 		else {
 			port.setMessage("Enter port number: Your input was invalid.");
 			port.setText("");
 		}
 	}
-	
+		
 	private boolean validateInput() {		
 		if (port.getText().isBlank())
 			return false;
@@ -122,5 +163,8 @@ public class HostScreen extends Screen {
 	private void closeServer() {
 		hControl.updateScreen(Mode.MAIN);
 		hControl.closeServer();
+		compositeNetwork.setEnabled(false);
+		compositeMap.setEnabled(true);
+		selectMap.setText("Select Map");
 	}
 }
