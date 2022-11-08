e8a75e1b41b7f958db5cc7419011a3725815d8a1
Nam <namdo1204@gmail.com>
2022-11-07 22:44:58 -0700

    20221107: Code redunancy removed.

--- A3_Xtank_SWT/GameScreen.java
+++ A3_Xtank_SWT/GameScreen.java
@@ -48,8 +48,7 @@ public class GameScreen extends Screen {
 		super(shell, display, cCon, hCon, cMod, hMod, mapID);
 	}
 	
-	@Override
-	protected Composite makeComposite(Shell shell, Display display) {
+	private void makeCompPart1(Shell shell, Display display) {
 		composite = new Composite(shell, SWT.COLOR_WHITE);
 		
 		compositePlayer = new Composite(composite, SWT.COLOR_WHITE);
@@ -77,42 +76,11 @@ public class GameScreen extends Screen {
 		compositeGame.setBackgroundMode(SWT.INHERIT_FORCE);
 		composite.setBackgroundMode(SWT.INHERIT_FORCE);
 
-		
 		canvas = new Canvas(compositeGame, SWT.TRANSPARENT);
-		map = new GameMap(display, compositeGame, 1);
-		
 		canvas.setBounds(0, 0, 800, 500);
-		
-		canvas.addPaintListener(event -> {
-			//Tank tank = cModel.getTank();
-			HashMap<Integer, Tank> tanks = cModel.getTanks();
-			for (var key : tanks.keySet()) {
-				Tank tank = tanks.get(key);
-				Rectangle tank_body = new Rectangle(tank.getX(), tank.getY(), tank.width, tank.height);
-				Transform transform = new Transform(display);
-				transform.translate(tank.getX() + (tank.width / 2), tank.getY() + (tank.height / 2));
-				transform.rotate(-(float)tank.getRotate() + 90.0f);
-				transform.translate(-tank.getX() - (tank.width / 2), -tank.getY() - (tank.height / 2));
-				event.gc.setTransform(transform);
-				//event.gc.fillRectangle(canvas.getBounds());
-				event.gc.setBackground(compositeGame.getDisplay().getSystemColor(SWT.COLOR_DARK_GREEN));
-				event.gc.fillRectangle(tank_body);
-				event.gc.setBackground(compositeGame.getDisplay().getSystemColor(SWT.COLOR_BLACK));
-				event.gc.fillOval(tank.getX(), tank.getY()+(tank.width/2), tank.width, tank.width);
-				event.gc.setLineWidth(4);
-				event.gc.drawLine(tank.getX()+(tank.width/2), tank.getY()+(tank.width/2), tank.getX()+(tank.width/2), tank.getY()-15);
-				//canvas.setBounds(tank.getX(), tank.getY(), 50, 100);
-				map.collision(tank.getX(), tank.getY(), tank.getX() + tank.height, tank.getY() + tank.height);
-			}
-			
-			ArrayList<Bullet> bullets = cModel.getBullets();
-			for (var bullet : bullets) {
-				Rectangle bullet_body = new Rectangle((int)bullet.getX(), (int)bullet.getY(), Bullet.size, Bullet.size);
-				event.gc.setBackground(compositeGame.getDisplay().getSystemColor(SWT.COLOR_DARK_GREEN));
-				event.gc.fillRectangle(bullet_body);
-			}
-		});
-		
+	}
+	
+	private void makeCompPart2() {
 		canvas.addMouseListener(new MouseListener() {
 			public void mouseDown(MouseEvent e) {
 				System.out.println("mouseDown in canvas");
@@ -173,46 +141,12 @@ public class GameScreen extends Screen {
 		canvas.setLayoutData(data2);
 		
 		composite.setLayout(layout);
-		
-		return composite;
 	}
 	
-	@Override
-	protected Composite makeCompositeAndMap(Shell shell, Display display, int mapID) {
-		composite = new Composite(shell, SWT.COLOR_WHITE);
-		
-		compositePlayer = new Composite(composite, SWT.COLOR_WHITE);
-		compositePlayer.setLayout(new FillLayout(SWT.VERTICAL));
-
-		plHeader = new Label(compositePlayer, SWT.BALLOON);
-		plHeader.setText("Players:");
-		plHeader.setFont(new Font(display,"Times New Roman", 14, SWT.BOLD ));
-		plHeader.setAlignment(SWT.LEFT);
-		
-		serverStatus = new Label(compositePlayer, SWT.BALLOON);
-		serverStatus.setText("Server\nACTIVE");
-		serverStatus.setFont(new Font(display,"Times New Roman", 12, SWT.BOLD ));
-		serverStatus.setAlignment(SWT.LEFT);
-		serverStatus.setForeground(display.getSystemColor(SWT.COLOR_GREEN));
-
-		
-		quit = new Button(compositePlayer, SWT.PUSH);
-		quit.setText("Quit");
-		quit.addSelectionListener(SelectionListener.widgetSelectedAdapter(e-> endGame()));
-		
-		compositeGame = new Composite(composite, SWT.COLOR_BLACK);
-		//compositeGame.setLayout(new FillLayout(SWT.VERTICAL));
-		//compositeGame.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
-		compositeGame.setBackgroundMode(SWT.INHERIT_FORCE);
-		composite.setBackgroundMode(SWT.INHERIT_FORCE);
-
-		
-		canvas = new Canvas(compositeGame, SWT.TRANSPARENT);
+	private void makeCompChange(Display display, int mapID, int tankID) {
 		map = new GameMap(display, compositeGame, mapID);
 		
-		canvas.setBounds(0, 0, 800, 500);
-		
-		canvas.addPaintListener(e -> {
+		canvas.addPaintListener(event -> {
 			//Tank tank = cModel.getTank();
 			HashMap<Integer, Tank> tanks = cModel.getTanks();
 			for (var key : tanks.keySet()) {
@@ -222,14 +156,14 @@ public class GameScreen extends Screen {
 				transform.translate(tank.getX() + (tank.width / 2), tank.getY() + (tank.height / 2));
 				transform.rotate(-(float)tank.getRotate() + 90.0f);
 				transform.translate(-tank.getX() - (tank.width / 2), -tank.getY() - (tank.height / 2));
-				e.gc.setTransform(transform);
+				event.gc.setTransform(transform);
 				//event.gc.fillRectangle(canvas.getBounds());
-				e.gc.setBackground(compositeGame.getDisplay().getSystemColor(SWT.COLOR_DARK_GREEN));
-				e.gc.fillRectangle(tank_body);
-				e.gc.setBackground(compositeGame.getDisplay().getSystemColor(SWT.COLOR_BLACK));
-				e.gc.fillOval(tank.getX(), tank.getY()+(tank.width/2), tank.width, tank.width);
-				e.gc.setLineWidth(4);
-				e.gc.drawLine(tank.getX()+(tank.width/2), tank.getY()+(tank.width/2), tank.getX()+(tank.width/2), tank.getY()-15);
+				event.gc.setBackground(compositeGame.getDisplay().getSystemColor(SWT.COLOR_DARK_GREEN));
+				event.gc.fillRectangle(tank_body);
+				event.gc.setBackground(compositeGame.getDisplay().getSystemColor(SWT.COLOR_BLACK));
+				event.gc.fillOval(tank.getX(), tank.getY()+(tank.width/2), tank.width, tank.width);
+				event.gc.setLineWidth(4);
+				event.gc.drawLine(tank.getX()+(tank.width/2), tank.getY()+(tank.width/2), tank.getX()+(tank.width/2), tank.getY()-15);
 				//canvas.setBounds(tank.getX(), tank.getY(), 50, 100);
 				map.collision(tank.getX(), tank.getY(), tank.getX() + tank.height, tank.getY() + tank.height);
 			}
@@ -237,71 +171,26 @@ public class GameScreen extends Screen {
 			ArrayList<Bullet> bullets = cModel.getBullets();
 			for (var bullet : bullets) {
 				Rectangle bullet_body = new Rectangle((int)bullet.getX(), (int)bullet.getY(), Bullet.size, Bullet.size);
-				e.gc.setBackground(compositeGame.getDisplay().getSystemColor(SWT.COLOR_DARK_GREEN));
-				e.gc.fillRectangle(bullet_body);
+				event.gc.setBackground(compositeGame.getDisplay().getSystemColor(SWT.COLOR_DARK_GREEN));
+				event.gc.fillRectangle(bullet_body);
 			}
 		});
-		
-		canvas.addMouseListener(new MouseListener() {
-			public void mouseDown(MouseEvent e) {
-				System.out.println("mouseDown in canvas");
-			} 
-			public void mouseUp(MouseEvent e) {} 
-			public void mouseDoubleClick(MouseEvent e) {} 
-		});
-
-			canvas.addKeyListener(new KeyListener() {
-			public void keyPressed(KeyEvent e) {
-				if (isConnected()) {
-
-					InputPacket packet = new InputPacket(0);
-					switch (e.character) {
-					case 'w':
-						packet.y = -1;
-						break;
-					case 's':
-						packet.y = 1;
-						break;
-					case 'a':
-						packet.x = -1;
-						break;
-					case 'd':
-						packet.x = 1;
-						break;
-					}
-					//System.out.println("key " + e.character);
-					// update tank location
-					//x += directionX;
-					//y += directionY;
-
-					cControl.writeOut(packet);
-					canvas.redraw();
-				}
-			}
-			public void keyReleased(KeyEvent e) {}
-		});
-
-		layout = new GridLayout();
-		layout.numColumns = 3;
+	}
 	
-		GridData data = new GridData();
-		data.horizontalSpan = 1;
-		data.grabExcessVerticalSpace = true;
-		//data.verticalAlignment = GridData.FILL;
-		data.heightHint = 400;
-		compositePlayer.setLayoutData(data);
+	@Override
+	protected Composite makeComposite(Shell shell, Display display) {
+		makeCompPart1(shell, display);
+		makeCompChange(display, 1, 1);
+		makeCompPart2();
 		
-		GridData data2 = new GridData();
-		data2.horizontalSpan = 2;
-		compositeGame.setLayoutData(data2);
-		
-		GridData data3 = new GridData();
-		data2.widthHint = 800;
-		data2.heightHint = 500;
-		data3.horizontalSpan = 2;
-		canvas.setLayoutData(data2);
-		
-		composite.setLayout(layout);
+		return composite;
+	}
+	
+	@Override
+	protected Composite makeCompositeAndMap(Shell shell, Display display, int mapID) {
+		makeCompPart1(shell, display);
+		makeCompChange(display, mapID, 1);
+		makeCompPart2();
 		
 		return composite;
 	}
